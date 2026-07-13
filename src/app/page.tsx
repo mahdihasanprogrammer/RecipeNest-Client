a65de36175a625408a27e41c309e76741d7f898f
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-07-13 11:09:12 +0600

    add faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FaqSection from "@/components/home/FaqSection";
 import HeroSection from "@/components/home/HeroSection";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between container mx-auto">
      <HeroSection />
+     <FaqSection />
     </div>
   );
 }
